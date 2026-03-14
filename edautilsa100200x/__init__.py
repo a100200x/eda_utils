69a9d5dd677164e25c7205101494e6de755c5a83
a100200x <a100200x@yandex.ru>
2026-03-14 12:05:19 +0300

    Update __init__.py

--- edautilsa100200x/__init__.py
+++ edautilsa100200x/__init__.py
@@ -3,6 +3,15 @@
 # Импортируем функцию из вложенного модуля
 from .styles.grafics import set_figure_gradient
 
+def use_style(style_name='style_1'):
+    """Применить стиль matplotlib из пакета"""
+    style_path = os.path.join(os.path.dirname(__file__), 'styles', f'{style_name}.mplstyle')
+    if os.path.exists(style_path):
+        plt.style.use(style_path)
+        print(f"Стиль {style_name} применен")
+    else:
+        print(f"Стиль {style_name} не найден")
+
 # Указываем, что доступно при импорте
 __all__ = ['set_figure_gradient']
 __version__ = '0.1'
