301ba7cbdd6339ad22d7844111d032b7bdcb457c
a100200x <a100200x@yandex.ru>
2026-03-14 12:35:43 +0300

    Update __init__.py

--- edautilsa100200x/__init__.py
+++ edautilsa100200x/__init__.py
@@ -1,10 +1,9 @@
 """EDA Utilities Package"""
 
 # Импортируем функцию из вложенного модуля
-from .styles.grafics import set_figure_gradient, use_style
-
+from .styles.grafics import set_figure_gradient, use_style, hex_to_rgba
 
 
 # Указываем, что доступно при импорте
-__all__ = ['set_figure_gradient', 'use_style']
+__all__ = ['set_figure_gradient', 'use_style', 'hex_to_rgba']
 __version__ = '0.1'
