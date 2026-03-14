c1bc42e0a4106ddae65fe32fc12a682d4aaa30ab
a100200x <a100200x@yandex.ru>
2026-03-14 12:13:21 +0300

    Update __init__.py

--- edautilsa100200x/__init__.py
+++ edautilsa100200x/__init__.py
@@ -1,18 +1,10 @@
 """EDA Utilities Package"""
 
 # Импортируем функцию из вложенного модуля
-from .styles.grafics import set_figure_gradient
-import os
+from .styles.grafics import set_figure_gradient, use_style
 
-def use_style(style_name='style_1'):
-    """Применить стиль matplotlib из пакета"""
-    style_path = os.path.join(os.path.dirname(__file__), 'styles', f'{style_name}.mplstyle')
-    if os.path.exists(style_path):
-        plt.style.use(style_path)
-        print(f"Стиль {style_name} применен")
-    else:
-        print(f"Стиль {style_name} не найден")
+
 
 # Указываем, что доступно при импорте
-__all__ = ['set_figure_gradient']
+__all__ = ['set_figure_gradient', 'use_style']
 __version__ = '0.1'
