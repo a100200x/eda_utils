7023fdaaa6e1efabf82ec2180fd86a44a8e1f79f
a100200x <a100200x@yandex.ru>
2026-03-14 09:52:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='edautilsa100200x',
-      version='0.0',
+      version='0.1',
       description='EDA_utilities',
       packages=['edautilsa100200x'],
       author_email='author@gmail.com',
