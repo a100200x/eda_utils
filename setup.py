278637341fbf44af390788861a97270c61272aa8
a100200x <a100200x@yandex.ru>
2026-03-14 09:33:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='eda_utils_a100200x',
+      version='0.0',
+      description='EDA_utilities',
+      packages=['eda_utils_a100200x'],
+      author_email='author@gmail.com',
+      zip_safe=False)
